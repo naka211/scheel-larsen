b00deb6aec61f08de31b3d0106c8464aa0765839
hoangtrieu2203@gmail.com <hoangtrieu2203@gmail.com@3793cc5f-04b3-b622-0fe1-ac11396135c4>
2014-08-24 12:30:09 +0000

    Update

---
+++ trunk/templates/scheellarsen/html/com_virtuemart/cart/mycart.php
@@ -0,0 +1,68 @@
+<?php
+/**
+*
+* Layout for the shopping cart
+*
+* @package	VirtueMart
+* @subpackage Cart
+* @author Max Milbers
+*
+* @link http://www.virtuemart.net
+* @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* @version $Id: cart.php 2551 2010-09-30 18:52:40Z milbo $
+*/
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<?php 
+$cart = VirtueMartCart::getCart(); 
+$cart->prepareCartViewData();
+?>
+<?php foreach($cart->products as $pid => $product){
+    $customhtml = $product->customfields;
+//    preg_match_all("#<span class=\"product-field-type-S\"> ([\w\W]*?)</span>#", $product->customfields, $tmp);
+//    $select1 = $tmp[1][0];
+//    $select2 = $tmp[1][1];
+//    preg_match("#src=\"([\w\W]*?)\" alt#", $product->customfields, $tmp1);
+//    $img = $tmp1[1];
+    preg_match("/<img .*?(?=src)src=\"([^\"]+)\" alt=\"([^\"]+)\"/si", $customhtml, $matches); 
+    $cusfinal1 = str_replace($matches[0]."  />", "", $customhtml);
+    $cusfinal = str_replace('<span class="product-field-type-M"> </span><br />', '', $cusfinal1);
+?>
+<li class="clearfix">
+    <div class="list-cart-img"><a href="<?php echo $product->url; ?>">
+        <?php 
+            if(count($matches)>0){
+                echo $matches[0].'/>';
+            }else{
+                echo $product->image->displayMediaThumb ('', FALSE);
+            }
+        ?>
+        </a></div>
+    <div class="list-cart-content">
+      <h4><?php echo $product->product_name; ?></h4>
+      <p>Vare-nummer: <?php echo $product->sku; ?></p>
+      <p><?php echo $cusfinal ?></p>
+    </div>
+    <div class="count">
+       <p><?php echo $product->quantity;?></p>
+    </div>
+    <p class="price">
+        <?php if ($cart->pricesUnformatted[$pid]['discountedPriceWithoutTax']) {
+                echo number_format($cart->pricesUnformatted[$pid]['discountedPriceWithoutTax'], 2,',','.'). ' DKK';
+        } else {
+                echo number_format($cart->pricesUnformatted[$pid]['basePriceVariant'], 2,',','.'). ' DKK';
+        }
+        ?>
+    </p>
+    <p class="price3"><?php echo number_format($cart->pricesUnformatted[$pid]['salesPrice']*$product->quantity, 2,',','.'). ' DKK' ?></p>
+    <a href="<?php echo JRoute::_ ('index.php?option=com_virtuemart&view=cart&task=delete&cart_virtuemart_product_id=' . $product->cart_item_id) ?>" rel="nofollow"" class="btnClose">close</a>
+</li>
+<?php } ?>
